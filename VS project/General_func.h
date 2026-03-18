b097020334b0a732e2390d4aa5cb6edb7fa12828
Fedor <po4tarev.fedor@yandex.ru>
2026-03-18 13:46:37 +0300

    Убран include vector, добавлен набросок функции работы меню

--- VS project/General_func.h
+++ VS project/General_func.h
@@ -1,14 +1,14 @@
 #include <iostream>
 #include <windows.h>
 #include <conio.h> //÷òåíèå êëâàèø
-#include <vector>
+
 
 #ifndef GENERAL_H
 #define GENERAL_H
 
 
 int print_menu(std::string menu[], int size); //ôóíêöèÿ âûâîäà íà ýêðàí ìåíþ è ðàáîòû ñ íèì
-
+int work_menu();
 
 
 
