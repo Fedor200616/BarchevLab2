54c2504b3f6d1cb2f647f2484fb47f33b0370215
Федор Почтарев PC <po4tarev.fedor@yandex.ru>
2026-03-18 20:17:28 +0300

    Добавлены комментарии улучшена работ меню

--- VS project/Work_with_files.cpp
+++ VS project/Work_with_files.cpp
@@ -1,27 +1,25 @@
 #include "Work_with_files.h"  
 
 fs::path FileDialog() {  
-
     wchar_t filename[MAX_PATH];  
 
-    OPENFILENAME ofn;  //обьявляем структуру
+    OPENFILENAME ofn;   //Структура OPENFILENAMEA (commdlg.h)
     ZeroMemory(&filename, sizeof(filename));  
     ZeroMemory(&ofn, sizeof(ofn));  
+
     ofn.lStructSize = sizeof(ofn);  
     ofn.hwndOwner = NULL;  
-    ofn.lpstrFilter = L"Text Files\0*.txt\0Any File\0*.*\0";  
+    ofn.lpstrFilter = L"Text Files\0*.txt\0*.*\0";  //фильтруем на текстовые
     ofn.lpstrFile = filename;  
     ofn.nMaxFile = MAX_PATH;  
-    ofn.lpstrTitle = L"Select a File";  
-    ofn.Flags = OFN_DONTADDTORECENT | OFN_FILEMUSTEXIST;  
+    ofn.lpstrTitle = L"Выберите файл";  //заголовок
+    ofn.Flags = OFN_DONTADDTORECENT | OFN_FILEMUSTEXIST; //Флаги не добавлять несуществующий, не делать последним 
 
-    if (GetOpenFileNameW(&ofn))  
-    {  
+    if (GetOpenFileNameW(&ofn)) {  
         std::wcout << L"You chose the file \"" << filename << L"\"\n";  
         return fs::path(filename);    
     }  
-    else  
-    {  
+    else {  
         std::wcout << L"You cancelled.\n"; 
         return "0";
     }  
